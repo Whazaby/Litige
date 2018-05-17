11983661c6c8b6ab6d47508b3ea52a58d73f186c
Guillaume Ruscassie <guillaume.ruscassie@gmail.com>
2018-05-17 22:38:16 +0200

    init back

---
+++ Litige-Back/src/main/java/com/ruscassie/guillaume/litige/LitigeApplication.java
@@ -0,0 +1,12 @@
+package com.ruscassie.guillaume.litige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitigeApplication.class, args);
+	}
+}
---
+++ Litige-Back/src/test/java/com/ruscassie/guillaume/litige/LitigeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ruscassie.guillaume.litige;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LitigeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
