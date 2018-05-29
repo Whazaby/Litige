fee07dd51eb1a9834bbc01b08be5ce11ecfde6ea
Ruscassie, Guillaume (DI CL) - AF (ext) <guruscassie-ext@airfrance.fr>
2018-05-29 11:35:10 +0200

    add logback

--- Litige-Back/src/main/java/com/ruscassie/guillaume/litige/LitigeApplication.java
+++ Litige-Back/src/main/java/com/ruscassie/guillaume/litige/LitigeApplication.java
@@ -1,12 +1,17 @@
 package com.ruscassie.guillaume.litige;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LitigeApplication {
+	private  static final Logger LOGGER = LoggerFactory.getLogger(LitigeApplication.class);
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LitigeApplication.class, args);
+		LOGGER.info("--- Starting Litige ---");
 	}
 }
--- Litige-Back/src/test/java/com/ruscassie/guillaume/litige/LitigeApplicationTests.java
+++ Litige-Back/src/test/java/com/ruscassie/guillaume/litige/LitigeApplicationTests.java
@@ -2,15 +2,19 @@ package com.ruscassie.guillaume.litige;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LitigeApplicationTests {
+	private final Logger LOGGER = LoggerFactory.getLogger(LitigeApplicationTests.class);
 
 	@Test
 	public void contextLoads() {
+		LOGGER.info("Test");
 	}
 
 }
