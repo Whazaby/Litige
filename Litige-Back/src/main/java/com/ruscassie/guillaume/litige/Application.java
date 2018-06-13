5aab9dba06b8509200cb0de789741433d33c8270
Ruscassie, Guillaume (DI CL) - AF (ext) <guruscassie-ext@airfrance.fr>
2018-06-13 11:36:03 +0200

    add simple login

--- Litige-Back/src/main/java/com/ruscassie/guillaume/litige/Application.java
+++ Litige-Back/src/main/java/com/ruscassie/guillaume/litige/Application.java
@@ -6,12 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LitigeApplication {
-	private  static final Logger LOGGER = LoggerFactory.getLogger(LitigeApplication.class);
+public class Application {
+	private  static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(LitigeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 		LOGGER.info("--- Starting Litige ---");
 	}
 }
