7d35cdc9c65410866e80adf426faf99b16ac6bb8
Guillaume Ruscassie <guillaume.ruscassie@gmail.com>
2018-07-01 22:28:31 +0200

    add basic security with default user

--- Litige-Back/src/main/java/com/ruscassie/guillaume/litige/WebSecurityConfiguration.java
+++ Litige-Back/src/main/java/com/ruscassie/guillaume/litige/WebSecurityConfiguration.java
@@ -1,46 +1,33 @@
 package com.ruscassie.guillaume.litige;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/", "/home", "/login").permitAll()
+				.anyRequest().authenticated().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+	
+	
+	  @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		  auth
+			.inMemoryAuthentication()
+				.passwordEncoder(NoOpPasswordEncoder.getInstance())
+				.withUser("user").password("password").roles("USER");
 
-        http
-                      .authorizeRequests()
-                      .antMatchers("/", "/home","/api/**").permitAll()
-                      .anyRequest().authenticated()
-                      .and()
-                      .formLogin()
-                      .loginPage("/login")
-                      .permitAll()
-                      .and()
-                      .logout()
-                      .permitAll();
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                      User.withDefaultPasswordEncoder()
-                                    .username("user")
-                                    .password("password")
-                                    .roles("USER")
-                                    .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
-
+	    }
+	 
 }
